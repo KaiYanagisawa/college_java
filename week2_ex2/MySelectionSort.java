a730d0e03a50bc5f15771e49f551fd8d4b325adc
KaiYanagisawa <12730ky@gmail.com>
2022-10-03 14:28:34 +0900

    finish_MySelectionSort

--- week2_ex2/MySelectionSort.java
+++ week2_ex2/MySelectionSort.java
@@ -1,31 +1,33 @@
 public class MySelectionSort {
 
-    public static void selectionSort(int[] data) {
-      for(int i=0;i<data.length;i++){
-          int min=i;
-  for(int j=i+1;j<data.length;j++){
-            if(data[min]>data[j]){
-              int n;
-              n=data[min];
-              data[min]=data[j];
-              data[j]=n;
-            }
-          }
+  public static void selectionSort(int[] data) {
+    for (int i = 0; i < data.length - 1; i++) {
+      int min = i;
+      for (int j = i + 1; j < data.length; j++) {
+        if (data[min] > data[j]) min = j;
       }
+      int n = data[i];
+      data[i] = data[min];
+      data[min] = n;
+
       printData(data);
     }
+  }
 
-  //   public static void printData(int[] data) {
-  //     // 配列の要素を列挙する
-  //     for (int i = 0; i < data.length; i++) System.out.print(data[i] + ", ");
-  //     System.out.println();
-  //   }
+  public static void printData(int[] data) {
+    // 配列の要素を列挙する
+    for (int i = 0; i < data.length; i++) System.out.print(
+      data[i] + ((i < data.length - 1) ? ", " : "")
+    );
+    System.out.println();
+  }
 
   public static void main(String[] args) {
-    // int dataSize = args.length; // データ数
+    int dataSize = args.length; // データ数
     int[] data; // データ保存用配列
     // コマンドラインから入力したデータを配列 data に格納する
-    data = new int[] { Integer.parseInt(args[0]) };
+    data = new int[dataSize];
+    for (int i = 0; i < dataSize; i++) data[i] = Integer.parseInt(args[i]);
     // 選択ソートを実行
     selectionSort(data);
   }
