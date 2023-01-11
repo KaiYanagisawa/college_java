81d6bfc0f789419b54f34574b5b0612a7dddba26
KaiYanagisawa <12730ky@gmail.com>
2023-01-11 20:05:09 +0900

    main インスタンス

--- week10/InterfaceFileTester.java
+++ week10/InterfaceFileTester.java
@@ -3,73 +3,125 @@ import java.util.ArrayList;
 
 // StudentAdapterクラスに実装するためのDetailインタフェース
 interface Detail {
-    
+  void printInfo();
+
+  int getSubjectScore(String sn);
+  // double getAverage();
 }
 
- // 教科クラス 
- class Subject {
-     private String subjectName;
-     private int point;
+// 教科クラス
+class Subject {
 
-     //コンストラクタ
-     Subject(String sn, int p) {
-         this.subjectName = sn;
-         this.point = p;
-     }
+  private String subjectName;
+  private int point;
 
-     // subjectName, pointのゲッター 
-     public String getSubjectName() {
-         return subjectName;
-     }
+  //コンストラクタ
+  Subject(String sn, int p) {
+    this.subjectName = sn;
+    this.point = p;
+  }
 
-     public int getPoint() {
-         return point;
-     }
- }
- 
- //  学生クラス 
- class Student {
-     String name;
-     Subject[] sList;
+  // subjectName, pointのゲッター
+  public String getSubjectName() {
+    return subjectName;
+  }
 
-     //  コンストラクタ
-     Student(String n, Subject[] sl) {
-         this.name = n;
-         this.sList = sl;
-     }
-     
-     //  学生の科目情報を返すメソッド
-     public String getInfo() {
-         String result = "";
-         result += "Name:" + name + "\n";
-         for (int i = 0; i < sList.length; i++) {
-             Subject s = sList[i];
-             result += "科目:" + s.getSubjectName() + "  点数:" + s.getPoint() + "\n";
-         }
-         return result;
-     }
- }
+  public int getPoint() {
+    return point;
+  }
+}
+
+//  学生クラス
+class Student {
+
+  String name;
+  Subject[] sList;
+
+  //  コンストラクタ
+  Student(String n, Subject[] sl) {
+    this.name = n;
+    this.sList = sl;
+  }
+
+  //  学生の科目情報を返すメソッド
+  public String getInfo() {
+    String result = "";
+    result += "Name:" + name + "\n";
+    for (int i = 0; i < sList.length; i++) {
+      Subject s = sList[i];
+      result += "科目:" + s.getSubjectName() + "  点数:" + s.getPoint() + "\n";
+    }
+    return result;
+  }
+}
 
 //Studentクラスを継承し、Detailインタフェースを実装したStudentAdapterクラス
-public class StudentAdapter implements Detail {
-    
+class StudentAdapter extends Student implements Detail {
+
+  StudentAdapter(String n, Subject[] sl) {
+    super(n, sl);
+  }
+
+  public int getSubjectScore(String sn) {
+    return 1;
+  }
+
+  // public double getAverage() {
+  //     return
+  // }
+
+  public void printInfo() {
+    System.out.println("Name:" + name);
+    for (int i = 0; i < sList.length; i++) {
+      Subject s = sList[i];
+      System.out.println(
+        "科目:" +
+        s.getSubjectName() +
+        " 点数:" +
+        getSubjectScore(s.getSubjectName()) +
+        "\n"
+      );
+    }
+  }
 }
 
 public class InterfaceFileTester {
-    public static void main(String[] args) {
-        ArrayList<StudentAdapter> studentList = new ArrayList<StudentAdapter>();
-        try {
-            //  データファイルを読み込み，学生ごとにStudentAdapterオブジェクトを作成し，4人分のデータをstudentListに格納
 
-        } catch (FileNotFoundException e) {
-            System.out.println("FileReaderから投げられた例外" + e);
-        } catch (IOException e) {
-            System.out.println("readLineから投げられた例外" + e);
-        }
-        for (StudentAdapter sa : studentList) {
-            sa.printInfo();
-            System.out.println("３教科の平均点:" + sa.getAverage() + "\n");
-        }
+  public static void main(String[] args) {
+    ArrayList<StudentAdapter> studentList = new ArrayList<StudentAdapter>();
+    try {
+      //  データファイルを読み込み，学生ごとにStudentAdapterオブジェクトを作成し，4人分のデータをstudentListに格納
+      BufferedReader reader = new BufferedReader(
+        new FileReader("StudentData_UTF-8.txt")
+      );
+      String line;
+      while ((line = reader.readLine()) != null) {
+        String[] s = line.split(",");
+        String name = s[0];
+        Subject math_points = new Subject("Math", Integer.parseInt(s[1]));
+        Subject english_points = new Subject("English", Integer.parseInt(s[2]));
+        Subject japanese_points = new Subject(
+          "Japanese",
+          Integer.parseInt(s[3])
+        );
+
+        Subject[] slist = new Subject[] {
+          math_points,
+          english_points,
+          japanese_points,
+        };
+
+        StudentAdapter student = new StudentAdapter(name, slist);
+      }
+      reader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("FileReaderから投げられた例外" + e);
+    } catch (IOException e) {
+      System.out.println("readLineから投げられた例外" + e);
     }
+    for (StudentAdapter sa : studentList) {
+      sa.printInfo();
+      // System.out.println("３教科の平均点:" + sa.getAverage() + "\n");
+    }
+  }
 }
-         
