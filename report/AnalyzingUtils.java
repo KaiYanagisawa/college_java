12c279e06c2f7083e68d4ad2901fdc1b290ade04
KaiYanagisawa <12730ky@gmail.com>
2023-01-12 12:45:49 +0900

    必要クラス準備

--- report/AnalyzingUtils.java
+++ report/AnalyzingUtils.java
@@ -1,5 +1,58 @@
+import java.util.*;
+
 public interface AnalyzingUtils {
   public abstract void loadData(String filename);
 
   public abstract int getChartIn(String kyoku);
+}
+
+class GeneralDatabase {
+  ArrayList<General_Item> DataList;
+
+  void printDatabase() {
+  }
+
+  int getTrackRanking(String s) {
+  }
+  
+  void getTop20Artists() {
+  }
+}
+
+class WeeklyDatabase extends GeneralDatabase implements AnalyzingUtils {
+  ArrayList<GeneralDatabase> DataList;
+
+  public void loadData(String filename) {
+  }
+
+  public int getChartIn(String kyoku) {
+  }
+  
+  void printDatabase() {
+  }
+
+  int getTrackRanking(String kyoku) {
+  }
+
+  void getTop20Artists() {
+  }
+}
+
+class DailyDatabase extends GeneralDatabase implements AnalyzingUtils {
+  ArrayList<GeneralDatabase> DataList;
+
+  public void loadData(String filename) {
+  }
+
+  public int getChartIn(String kyoku) {
+  }
+  
+  void printDatabase() {
+  }
+
+  int getTrackRanking(String kyoku) {
+  }
+
+  void getTop20Artists() {
+  }
 }
--- report/General_Item.java
+++ report/General_Item.java
@@ -1,22 +1,24 @@
 public abstract class General_Item {
 
-  int rank;
-  String artist_names;
-  String track_name;
-  String source;
-  int peak_rank;
-  int previous_rank;
-  int streams;
+    int rank;
+    String artist_names;
+    String track_name;
+    String source;
+    int peak_rank;
+    int previous_rank;
+    int streams;
 
-  General_Item() {
-    this.rank = -1;
-    this.artist_names = "noname";
-    this.track_name = "noname";
-    this.source = "nosource";
-    this.peak_rank = -1;
-    this.previous_rank = -1;
-    this.streams = -1;
-  }
+    General_Item() {
+        this.rank = -1;
+        this.artist_names = "noname";
+        this.track_name = "noname";
+        this.source = "nosource";
+        this.peak_rank = -1;
+        this.previous_rank = -1;
+        this.streams = -1;
+    }
 
-  abstract void printData();
+    abstract void printData();
 }
+
+
