3b91558207713c61b9b10919b6fb5374bc994339
KaiYanagisawa <12730ky@gmail.com>
2023-01-12 17:01:01 +0900

    データ内容表示実装

--- report/AnalyzingUtils.java
+++ report/AnalyzingUtils.java
@@ -13,7 +13,7 @@ class GeneralDatabase {
   ArrayList<General_Item> DataList;
 
   void printDatabase() {
-    for (int i = 1; i < DataList.size(); i++) {
+    for (int i = 0; i < DataList.size(); i++) {
       DataList.get(i).printData();
     }
   }
@@ -45,7 +45,8 @@ class WeeklyDatabase extends GeneralDatabase implements AnalyzingUtils {
           songData.peak_rank = Integer.parseInt(s[5]);
           songData.previous_rank = Integer.parseInt(s[6]);
           songData.weeks_on_chart = Integer.parseInt(s[7]);
-          songData.streams = Integer.parseInt(s[8].substring(1, s[8].length()-1));
+          songData.streams =
+            Integer.parseInt(s[8].substring(1, s[8].length() - 1));
         }
 
         lineNumber++;
@@ -65,7 +66,7 @@ class WeeklyDatabase extends GeneralDatabase implements AnalyzingUtils {
   }
 
   void printDatabase() {
-    for (int i = 0; i < DataList.size(); i++) {
+    for (int i = 1; i < DataList.size(); i++) {
       DataList.get(i).printData();
     }
   }
@@ -79,15 +80,49 @@ class WeeklyDatabase extends GeneralDatabase implements AnalyzingUtils {
 
 class DailyDatabase extends GeneralDatabase implements AnalyzingUtils {
 
-  ArrayList<General_Item> DataList;
+  ArrayList<General_Item> DataList = new ArrayList<General_Item>();
 
-  public void loadData(String filename) {}
+  public void loadData(String filename) {
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(filename));
+      String line;
+      int lineNumber = 0;
+      while ((line = reader.readLine()) != null) {
+        String[] s = line.split(",");
+        Daily_Item songData = new Daily_Item();
+        if (lineNumber != 0) {
+          songData.rank = Integer.parseInt(s[0]);
+          songData.artist_names = s[2];
+          songData.track_name = s[3];
+          songData.source = s[4];
+          songData.peak_rank = Integer.parseInt(s[5]);
+          songData.previous_rank = Integer.parseInt(s[6]);
+          songData.days_on_chart = Integer.parseInt(s[7]);
+          songData.streams =
+            Integer.parseInt(s[8].substring(1, s[8].length() - 1));
+        }
+
+        lineNumber++;
+
+        DataList.add(songData);
+      }
+      reader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println(e);
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+  }
 
   public int getChartIn(String kyoku) {
     return 0;
   }
 
-  void printDatabase() {}
+  void printDatabase() {
+    for (int i = 1; i < DataList.size(); i++) {
+      DataList.get(i).printData();
+    }
+  }
 
   int getTrackRanking(String kyoku) {
     return 0;
